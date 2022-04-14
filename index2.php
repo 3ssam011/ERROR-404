3bb860b98da02d4237b5d8a0a58f1014bff8b415
AmmarWael <ammar.wael1@msa.edu.eg>
2022-04-14 18:02:21 +0200

    css

---
+++ index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Managment System</title>
+    <link rel="stylesheet" href="login2.css" title="style">
+</head>
+
+<body>
+    <form class="box" action="login.html" method="POST">
+        <h1> login </h1>
+        <input type="text" name="" placeholder="Enter username" id="username">
+        <input type="password" name="" placeholder="Enter password" id="password">
+        <input type="submit" name="" vale="login">
+    </form>
+</body>
+
+</html>
