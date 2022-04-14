f3b6c0991bd9f167dce333566bdd41b757a899a9
Elmansoory <ahmed.mohamed356@msa.edu.eg>
2022-04-14 13:25:09 +0200

    index

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scool Managment System</title>
+    <link rel="stylesheet"$type="text/css" href="g.css" title="style">
+</head>
+<body>
+    <div id="All">
+        
+        <head>
+            <div id="Welcome">
+                <p>
+                    <h1>
+                        Welcome To School System
+                    </h1>                
+                </p>
+            </div>
+        </head>
+        
+            <form action="login/login.php" method="POST">
+                <div id="choose">
+                    <label for="Teacher"> Teacher</label>
+                    <input type="radio" name="type" value="T">
+                    <label for="Student">Student</label>
+                    <input type="radio" name="type" value="S">
+                    <label for="Manager">Manager</label>
+                    <input type="radio" name="type" value="M">
+                </div>
+                <div id="id">           
+                    <label> Enter your ID</label>
+                    <span id="ID_TextBox">
+                        <input type="text" placeholder="ID" name="ID" >
+                    </span>                            
+                </div>
+                <div id="password">
+                    <label >Enter your Password</label>
+                    <span id="Pass_TextBox">
+                        <input type="password" placeholder="password" name="Password">
+                    </span>                               
+                </div>
+                <input type="submit">
+            </form> 
+            
+    </div>
+    
+    </body>
+
+</html>
