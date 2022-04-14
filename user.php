a293723a96f3b6bc6efda32eb616bf3a8056b15a
3ssam011 <100770844+3ssam011@users.noreply.github.com>
2022-04-14 15:17:10 +0200

    Update user.php

--- user.php
+++ user.php
@@ -2,12 +2,12 @@
 include_once"CRUD.php";
 class user
 {
-  public  $Fullname;
-  public $ID;
-  public $Password;
-  public $type;
-  public $email;
-  public $DOB;
+  private  $Fullname;
+  private $ID;
+  private $Password;
+  private $type;
+  private $email;
+  private $DOB;
   public function log($filename,$id,$pass)
   {
     $line=getRowById($filename, '~', $id);
@@ -28,5 +28,107 @@ class user
       echo'no';
     }
   }
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getDOB() {
+		return $this->DOB;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $DOB 
+	 * @return user
+	 */
+	function setDOB($DOB): self {
+		$this->DOB = $DOB;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getFullname() {
+		return $this->Fullname;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $Fullname 
+	 * @return user
+	 */
+	function setFullname($Fullname): self {
+		$this->Fullname = $Fullname;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getID() {
+		return $this->ID;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $ID 
+	 * @return user
+	 */
+	function setID($ID): self {
+		$this->ID = $ID;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getPassword() {
+		return $this->Password;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $Password 
+	 * @return user
+	 */
+	function setPassword($Password): self {
+		$this->Password = $Password;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getType() {
+		return $this->type;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $type 
+	 * @return user
+	 */
+	function setType($type): self {
+		$this->type = $type;
+		return $this;
+	}
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getEmail() {
+		return $this->email;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $email 
+	 * @return user
+	 */
+	function setEmail($email): self {
+		$this->email = $email;
+		return $this;
+	}
 }
 ?>
