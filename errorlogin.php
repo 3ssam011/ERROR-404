f8c57bff54fb6d5eb54f3f2aeae268218658e552
Elmansoory <ahmed.mohamed356@msa.edu.eg>
2022-04-16 22:41:35 +0200

    error

--- errorlogin.php
+++ errorlogin.php
@@ -11,7 +11,7 @@
     <h1>
         No ID Or Password likethat 
     </h1>
-    <form action="indexMain.php">
+    <form action="index.php">
         <button>
             back to login
         </button>
