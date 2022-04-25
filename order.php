8d013a028df8fee0d9ea4e99b75eb37ae32bd6b5
3ssam011 <100770844+3ssam011@users.noreply.github.com>
2022-04-25 23:48:41 +0200

    a

---
+++ order.php
@@ -0,0 +1,6 @@
+<?php
+class order {
+    public $fullname;
+    public $id;
+}
+?>
---
+++ orderDetail.php
@@ -0,0 +1,19 @@
+<?php
+ include_once "order.php";
+class orderDetails extends order{
+    public $date;
+    public $email;
+    public $phoneNumber;
+    public $Grade;
+    public function orderDetails($name,$id,$Grade,$email,$date)
+    {
+            $filename="order.txt";
+            $this->Fullname=$name;
+            $this->id=$id;
+            $this->Grade=$Grade;
+            $this->email=$email;
+            $this->date=$date;
+            ADD($filename,$this->email,$this->id, $this->Fullname,$this->date);
+    }
+}
+?>
