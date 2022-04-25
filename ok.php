c787e725962691c800cb5b4ead8d67acc48e4db6
bebo100s <mahmoud.mohamed90@msa.edu.eg>
2022-04-25 22:24:14 +0200

    mm

--- ok.php
+++ ok.php
@@ -5,5 +5,10 @@ $email=$_POST['Email'];
 $pass="Ihf".$dob;
 $type=$_POST['type'];
 $check=$_POST['choose'];
+if($check=="F")
+{
+    $pt= new manager ();
+    $pt->FiresUser($name,$pass,$email,$DOB,$id)
 
+}
 ?>
