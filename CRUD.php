0b2852cd3e0f8e1a90a42ba0e22f5628c8c6c693
Elmansoory <ahmed.mohamed356@msa.edu.eg>
2022-05-15 20:59:07 +0200

    hashing

--- CRUD.php
+++ CRUD.php
@@ -1,4 +1,22 @@
 <?php
+function Encrypt($Word, $Key)
+{
+    $Result = "";
+    for ($i = 0; $i < strlen($Word); $i++) {
+        $c = chr(ord($Word[$i]) + $Key + $i);
+        $Result .= $c;
+    }
+    return $Result;
+}
+function Decrypt($Word, $Key)
+{
+    $Result = "";
+    for ($i = 0; $i < strlen($Word); $i++) {
+        $c = chr(ord($Word[$i]) - $Key - $i);
+        $Result .= $c;
+    }
+    return $Result;
+}
 function StoreRecord($fileName, $record)
 {
     $myfile = fopen($fileName, "a+");
