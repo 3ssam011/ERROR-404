b5e5f42ec34ecbda270788baad3aa013c09595c9
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2022-06-06 23:24:59 +0200

    #on hire

--- Hire.php
+++ Hire.php
@@ -1,12 +1,13 @@
 <?php
 include_once "manager.php";
+include_once "CRUD.php";
 $Fullname=$_POST['FullName'];
 $id="1";
 //$_POST['ID'];
 $dob=$_POST['Date'];
 $email=$_POST['Email'];
 $pass="Ihf".$dob;
-
+$password=Encrypt($pass,1);
 $type=$_POST['type'];
 //$_POST['choose'];
 $check="H";
@@ -15,7 +16,7 @@ $pt->Fullname=$Fullname;
 $pt->email=$email;
 $pt->ID=$id;
 $pt->DOB=$dob;
-$pt->Password=$pass;
+$pt->Password=$password;
 $pt->type=$type;
 if($check=="H")
 {    
