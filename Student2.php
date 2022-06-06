d386811b6aed3cc76f3d70ff08b4c83c2a06ee6f
AmmarWael <ammar.wael1@msa.edu.eg>
2022-06-06 20:29:27 +0200

    0

--- Student2.php
+++ Student2.php
@@ -1,8 +1,12 @@
 <?php
 include_once "Observer.php";
-class student22 implements Observer{
+class student2 implements Observer{
     private $name;
-    
+
+    public function student2($name){
+        $this->name=$name;
+    }
+
     public function update($message){
         echo($this->name + " has new notification " + $message);
     }
