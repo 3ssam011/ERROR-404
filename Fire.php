7a7e6bc3a042dfe56e5411314f94fb8238447089
3ssam011 <100770844+3ssam011@users.noreply.github.com>
2022-06-07 13:19:36 +0200

    v

--- Fire.php
+++ Fire.php
@@ -3,19 +3,19 @@ include_once "manager.php";
 $Fullname=$_POST['FullName'];
 $id="1";
 //$_POST['ID'];
-$dob=$_POST['Date'];
-$email=$_POST['Email'];
-$pass="Ihf".$dob;
+//$dob=$_POST['Date'];
+//$email=$_POST['Email'];
+//$pass="Ihf".$dob;
 
 $type=$_POST['type'];
 //$_POST['choose'];
 $check="F";
 $pt=new manager();
 $pt->Fullname=$Fullname;
-$pt->email=$email;
+//$pt->email=$email;
 $pt->ID=$id;
-$pt->DOB=$dob;
-$pt->Password=$pass;
+//$pt->DOB=$dob;
+//$pt->Password=$pass;
 $pt->type=$type;
 if($check=="F")
 {    
