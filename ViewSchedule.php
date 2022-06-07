5d5a30126870d6e0126c95f165ee36619ad85b49
AmmarWael <ammar.wael1@msa.edu.eg>
2022-06-07 15:46:01 +0200

    student schedule final

--- ViewSchedule.php
+++ ViewSchedule.php
@@ -1,6 +1,8 @@
 <?php
  include_once "fileTable.php";
+ session_start(); 
+ $xx=$_SESSION['IDM'];
  $x=new file();
  $x->filename="orderDetails.txt";
- $x->DrawScedule($x);
+ $x->DrawScedule($x,$xx);
 ?>
--- fileTable.php
+++ fileTable.php
@@ -1,4 +1,5 @@
 <?php
+include_once"CRUD.php";
 class file{
     public $filename;
     public function drawtable()
@@ -19,27 +20,36 @@ class file{
         
     }
 
-    public function DrawScedule($x)
+    public function DrawScedule($x,$xx)
     {
+        $ss=0;
         $this->filename=$x->filename;
         $myfile=fopen($this->filename,"r") or die("sorry cannot be opened");
+        $linee= getRowById("student.txt", '~', $xx);
+        $arrayline=explode('~',$linee);
+        $record= $arrayline[1];
         while(!feof($myfile))
         {
             $line=fgets($myfile);            
             $array=explode("~",$line);
             for($i=0;$i<count($array);$i++)
-            {            
-                if($i+1==count($array))   
+            { 
+                if($array[1]==$record)
                 {
-                    echo $array[$i];
-                }                
+                    if($i+1==count($array))   
+                    {
+                        echo "Your cources are: ";
+                        echo $array[$i];
+                        $ss=1;
+                    }
+                }                     
             }
-            echo"<br>";
-
-            
+        }
+        if($ss==0)
+        {
+            echo "No student with the same name";
         }
         fclose($myfile);
-        
     }
 
 	/**
